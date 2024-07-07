761669c7a6bc8ba6c04d028eeae26b98366f557a
Neha_Kumari711 <127786136+Nkovaturient@users.noreply.github.com>
2024-07-07 10:47:28 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url=`http://localhost:4000`
+  const url=`https://xomato-food-delivery-app.onrender.com` //http://localhost:4000
 
   return (
     <div>
