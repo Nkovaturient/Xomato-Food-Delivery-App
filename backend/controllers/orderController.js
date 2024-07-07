76bc81d50462bbb1e06898f2577b5722bff9d688
Neha_Kumari711 <127786136+Nkovaturient@users.noreply.github.com>
2024-07-07 10:57:57 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe= new Stripe(process.env.STRIPE_SECRET_KEY);
 //placing user order for frontend
 const placeOrder=async(req, res)=>{
 
-    const frontend_URL=`http://localhost:5173`;
+    const frontend_URL=`https://xomato-1x12.onrender.com`; //http://localhost:5173
 
     try{
         const newOrder= new orderModel({
